c6f734a06ab381cc01dfa95c9952da9cc38b792b
Sven Van Caekenberghe <sven@stfx.eu>
2026-08-18 16:05:40 +0200

    include utils.ex

---
+++ lib/blueos_now_playing/utils.ex
@@ -0,0 +1,36 @@
+defmodule BlueOSNowPlaying.Utils do
+  def ip_to_string(ip) when is_tuple(ip) do
+    ip
+    |> Tuple.to_list()
+    |> Enum.map(&to_string(&1))
+    |> Enum.join(".")
+  end
+
+  def string_to_ip(string) when is_binary(string) do
+    string
+    |> String.split(".")
+    |> Enum.map(fn s ->
+      {i, ""} = Integer.parse(s)
+      i
+    end)
+    |> List.to_tuple()
+  end
+
+  def format_time(secs) when is_integer(secs) do
+    if secs < 3600 do
+      minutes = div(secs, 60)
+      seconds = rem(secs, 60)
+      "#{format_2digits(minutes)}:#{format_2digits(seconds)}"
+    else
+      hours = div(secs, 3600)
+      rem = rem(secs, 3600)
+      minutes = div(rem, 60)
+      seconds = rem(rem, 60)
+      "#{format_2digits(hours)}:#{format_2digits(minutes)}:#{format_2digits(seconds)}"
+    end
+  end
+
+  defp format_2digits(int) when is_integer(int) and int >= 0 do
+    int |> to_string() |> String.pad_leading(2, "0")
+  end
+end
