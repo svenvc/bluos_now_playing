cb712570197021d8acba33eb87bf59353afeb83d
Sven Van Caekenberghe <sven@stfx.eu>
2026-08-18 11:54:49 +0200

    make NowPlaying count seconds

--- lib/blueos_now_playing_web/live/now_playing.ex
+++ lib/blueos_now_playing_web/live/now_playing.ex
@@ -6,28 +6,26 @@ defmodule BlueOSNowPlayingWeb.NowPlaying do
     ~H"""
     <div class="bg-sky-500 bg-cover bg-center h-screen grid justify-center">
       <h1 class="text-9xl font-bold text-neutral-50 mt-20 mb-20">Now Playing</h1>
-      <p class="text-xl text-neutral-50 mb-20 font-mono">Server clock: {@server_clock}</p>
+      <p class="text-xl text-neutral-50 mb-20 font-mono">Seconds: {@secs}</p>
     </div>
     """
   end
 
   @impl true
   def mount(_params, _session, socket) do
-    now = DateTime.utc_now(:second)
-
     if connected?(socket) do
-      Process.send_after(self(), :update_clock, 1000)
+      Process.send_after(self(), :update_secs, 1000)
     end
 
-    {:ok, assign(socket, page_title: "Now Playing", server_clock: now)}
+    {:ok, assign(socket, page_title: "Now Playing", secs: 0)}
   end
 
   @impl true
-  def handle_info(:update_clock, socket) do
-    now = DateTime.utc_now(:second)
+  def handle_info(:update_secs, socket) do
+    %{assigns: %{secs: secs}} = socket
 
-    Process.send_after(self(), :update_clock, 1000)
+    Process.send_after(self(), :update_secs, 1000)
 
-    {:noreply, assign(socket, server_clock: now)}
+    {:noreply, assign(socket, secs: secs + 1)}
   end
 end
