6470d77e04518f8bc4bbe61bb88ce09e95ed60f0
Sven Van Caekenberghe <sven@stfx.eu>
2026-08-26 14:26:21 +0200

    update deps

--- mix.exs
+++ mix.exs
@@ -58,7 +58,7 @@ defmodule BluOSNowPlaying.MixProject do
       {:telemetry_metrics, "~> 1.0"},
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
-      {:dns_cluster, "~> 0.2.0"},
+      {:dns_cluster, "~> 0.3.0"},
       {:bandit, "~> 1.5"},
       {:req, "~> 0.7.0"},
       {:sweet_xml, "~> 0.7.0"}
