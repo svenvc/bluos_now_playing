85f36b9efb7300e1224db0fdf299af2eb5f48e46
Sven Van Caekenberghe <sven@beta9.be>
2026-09-06 19:51:50 +0200

    add unit tests for Utils format_time

--- test/bluos_now_playing/utils_test.exs
+++ test/bluos_now_playing/utils_test.exs
@@ -34,6 +34,38 @@ defmodule BluOSNowPlaying.UtilsTest do
     end
   end
 
+  describe "format_time/1" do
+    test "formats zero as MM:SS" do
+      assert Utils.format_time(0) == "00:00"
+    end
+
+    test "formats sub-minute seconds" do
+      assert Utils.format_time(9) == "00:09"
+      assert Utils.format_time(59) == "00:59"
+    end
+
+    test "formats minutes as MM:SS" do
+      assert Utils.format_time(60) == "01:00"
+      assert Utils.format_time(61) == "01:01"
+      assert Utils.format_time(671) == "11:11"
+    end
+
+    test "switches to H:MM:SS at exactly one hour" do
+      assert Utils.format_time(3599) == "59:59"
+      assert Utils.format_time(3600) == "01:00:00"
+      assert Utils.format_time(7385) == "02:03:05"
+    end
+
+    test "keeps hours beyond two digits" do
+      assert Utils.format_time(360_000) == "100:00:00"
+    end
+
+    test "handles negative input without raising" do
+      assert Utils.format_time(-9) == "00:09"
+      assert Utils.format_time(-3600) == "60:00"
+    end
+  end
+
   describe "round trip" do
     test "tuple -> string -> tuple is identity" do
       for ip <- [{0, 0, 0, 0}, {1, 2, 3, 4}, {192, 168, 178, 95}, {255, 255, 255, 255}] do
